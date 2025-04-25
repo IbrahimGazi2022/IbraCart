ed3b30dc80d27695fef7b82c1c1c76011da443e5
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2025-04-25 20:29:30 +0600

    Make Product Details Page

--- ibracart/lib/actions/product.actions.ts
+++ ibracart/lib/actions/product.actions.ts
@@ -2,6 +2,7 @@
 import { PrismaClient } from '@prisma/client';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import prisma from '@/db/prisma';  
 
 // Get the latest products
 export async function getLatestProducts() {
@@ -13,4 +14,11 @@ export async function getLatestProducts() {
     });
 
     return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: { slug: slug },
+    });
 }
