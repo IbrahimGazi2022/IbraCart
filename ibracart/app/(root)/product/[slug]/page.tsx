f9aa52ab59d298689e81ef26c8671d49b3030b3d
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2025-04-29 16:01:57 +0600

    Initial Deployment

--- ibracart/app/(root)/product/[slug]/page.tsx
+++ ibracart/app/(root)/product/[slug]/page.tsx
@@ -1,17 +1,15 @@
-import { notFound } from 'next/navigation';
-import ProductPrice from '@/components/shared/product/product-price';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { getProductBySlug } from '@/lib/actions/product.actions';
-import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
-import ProductImages from '@/components/shared/product/product-image';
+import { notFound } from 'next/navigation';
+import ProductPrice from '@/components/shared/product/product-price';
+import ProductImages from '@/components/shared/product/product-images';
 
 const ProductDetailsPage = async (props: {
     params: Promise<{ slug: string }>;
 }) => {
-    const params = await props.params;
-
-    const { slug } = params;
+    const { slug } = await props.params;
 
     const product = await getProductBySlug(slug);
     if (!product) notFound();
@@ -22,9 +20,8 @@ const ProductDetailsPage = async (props: {
                 <div className='grid grid-cols-1 md:grid-cols-5'>
                     {/* Images Column */}
                     <div className='col-span-2'>
-                        <ProductImages images={product.images!} />
+                        <ProductImages images={product.images} />
                     </div>
-
                     {/* Details Column */}
                     <div className='col-span-2 p-5'>
                         <div className='flex flex-col gap-6'>
@@ -33,10 +30,9 @@ const ProductDetailsPage = async (props: {
                             </p>
                             <h1 className='h3-bold'>{product.name}</h1>
                             <p>
-                                {product.rating.toString()} of {product.numReviews} reviews
+                                {product.rating.toString()} of {product.numReviews} Reviews
                             </p>
-
-                            <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
+                            <div className='flex flex-col sm:flex-row sm:items-center gap-3'>
                                 <ProductPrice
                                     value={Number(product.price)}
                                     className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2'
@@ -44,7 +40,7 @@ const ProductDetailsPage = async (props: {
                             </div>
                         </div>
                         <div className='mt-10'>
-                            <p>Description:</p>
+                            <p className='font-semibold'>Description</p>
                             <p>{product.description}</p>
                         </div>
                     </div>
@@ -61,14 +57,14 @@ const ProductDetailsPage = async (props: {
                                 <div className='mb-2 flex justify-between'>
                                     <div>Status</div>
                                     {product.stock > 0 ? (
-                                        <Badge variant='outline'>In stock</Badge>
+                                        <Badge variant='outline'>In Stock</Badge>
                                     ) : (
-                                        <Badge variant='destructive'>Unavailable</Badge>
+                                        <Badge variant='destructive'>Out Of Stock</Badge>
                                     )}
                                 </div>
                                 {product.stock > 0 && (
-                                    <div className=' flex-center'>
-                                        <Button className='w-full'>Add to cart</Button>
+                                    <div className='flex-center'>
+                                        <Button className='w-full'>Add To Cart</Button>
                                     </div>
                                 )}
                             </CardContent>
